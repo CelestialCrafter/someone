4d64066a6671e4bcd79b759dc2633205a89b0d9c
CelestialCrafter <celestialcraftermc@gmail.com>
2019-07-18 16:14:20 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+  if (message.author.bot && message.author.id !== client.user.id) return;
+  if (message.author.id == client.user.id) message.delete();
+  else {
+    let i = [];
+    if (message.content.search(/<\@538281576879095818\>/g) !== -1) {
+      message.guild.members.forEach(member => {
+        if (member.user.presence.status !== 'offline') return;
+        i.push(member.id);
+      });
+
+      let outputid = i[Math.floor(Math.random() * (i.length - 1) + 1)];
+      message.channel.send('<@'+outputid+'>');
+    }
+  }
+});
+
+client.on('ready', () => {
+  client.user.setActivity('Use @someone to ping someone!');
+});
+
+client.login('token uwu');
+console.log('ok ok ok i work');
